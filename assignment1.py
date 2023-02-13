b25f0eeec20e767290571d4bc63e4f24162e0097
Bas Soewanto <carharttpapi2000@gmail.com>
2023-02-13 12:53:42 +0100

    test1

---
+++ assignment1.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+
+from bokeh.io import output_file, output_notebook
+from bokeh.plotting import figure, show
+from bokeh.models import ColumnDataSource
+from bokeh.layouts import row, column, gridplot
+from bokeh.models.widgets import Tabs, Panel
